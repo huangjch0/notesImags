7cafe8a802b296a2ca308c7e947b9e8d19cda720
huangjch <huangjch16@outlook.com>
2024-03-02 21:51:44 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QFileDialog, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QFileDialog, QCheckBox, QGroupBox
 from PyQt5.QtCore import Qt
 from data_processor import DataCut, parse_opt
 import argparse
@@ -67,18 +67,49 @@ class LogProcessorApp(QWidget):
         layout.addWidget(self.keyword_label)
         layout.addWidget(self.keyword_input)
         layout.addWidget(self.sort_input)
-        layout.addWidget(self.delimiter_label)
-        layout.addWidget(self.delimiter_input)
-        layout.addWidget(self.table_nameP_label)
-        layout.addWidget(self.table_nameP_input)
-        layout.addWidget(self.notion_label)
-        layout.addWidget(self.notion_input)
+        # layout.addWidget(self.delimiter_label)
+        # layout.addWidget(self.delimiter_input)
+        
+       # 创建可折叠区域的组件
+        self.collapsible_box = QGroupBox("高级选项")
+        self.collapsible_box_layout = QVBoxLayout()
+
+        # 向可折叠区域的布局添加组件
+        self.collapsible_box_layout.addWidget(self.delimiter_label)
+        self.collapsible_box_layout.addWidget(self.delimiter_input)
+        self.collapsible_box_layout.addWidget(self.table_nameP_label)
+        self.collapsible_box_layout.addWidget(self.table_nameP_input)
+        self.collapsible_box_layout.addWidget(self.notion_label)
+        self.collapsible_box_layout.addWidget(self.notion_input)
+
+        # 设置可折叠区域的布局
+        self.collapsible_box.setLayout(self.collapsible_box_layout)
+
+        # 创建控制折叠的按钮
+        self.toggle_button = QPushButton("展开/收起高级选项")
+        self.toggle_button.clicked.connect(self.toggle_collapsible_box)
+
+        # 向主布局添加控制折叠的按钮和可折叠区域
+        layout.addWidget(self.toggle_button)
+        layout.addWidget(self.collapsible_box)
+        self.collapsible_box.hide()
+        # layout.addWidget(self.table_nameP_label)
+        # layout.addWidget(self.table_nameP_input)
+        # layout.addWidget(self.notion_label)
+        # layout.addWidget(self.notion_input)
         layout.addWidget(self.start_button)
         layout.addWidget(self.prompt_label)
         self.prompt_label.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.output_text)
-
+        
+        
         self.setLayout(layout)
+
+
+
+    def toggle_collapsible_box(self):
+        # 如果可折叠区域是可见的，则隐藏它，反之亦然
+        self.collapsible_box.setHidden(not self.collapsible_box.isHidden())
 
     # def browse_files(self):
     #     file_dialog = QFileDialog()
